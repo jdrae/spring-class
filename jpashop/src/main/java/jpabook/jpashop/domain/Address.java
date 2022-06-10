e4d10c86b4678ec8b161081ca999a9a6b645c233
jdrae <draejang@gmail.com>
2022-06-10 12:33:56 +0900

    order business logic

--- jpashop/src/main/java/jpabook/jpashop/domain/Address.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,11 +1,16 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter // 값 타입은 변경 불가능하게 해야한다. 생성자로 접근. 기본생성자 protected(jpa 위해)
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
     private String city;
